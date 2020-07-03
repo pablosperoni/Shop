03eb8875d92d8212ced5e7630a06174302f0972f
Pablo Speroni <pablolsperoni@gmail.com>
2020-07-03 11:39:48 -0300

    Part 09

---
+++ Shop.Web/Controllers/Products1Controller.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Shop.Web.Data;
+using Shop.Web.Data.Entities;
+
+namespace Shop.Web.Controllers
+{
+    public class Products1Controller : Controller
+    {
+        private readonly IRepository repository;
+
+        public Products1Controller(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        // GET: Products1
+        public IActionResult Index()
+        {
+            return View(repository.GetProducts());
+        }
+
+        // GET: Products1/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = repository.GetProduct(id.Value);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // GET: Products1/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products1/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.AddProduct(product);
+                await repository.SaveAllAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        // GET: Products1/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products1/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Price,ImageUrl,LastPurchase,LastSale,IsAvailable,Stock")] Product product)
+        {
+            if (id != product.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        // GET: Products1/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Products1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Products.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Shop.Web/Data/SeedDb.cs
@@ -0,0 +1,46 @@
+﻿namespace Shop.Web.Data
+
+{
+
+    using Shop.Web.Data.Entities;
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class SeedDb
+    {
+        private readonly DataContext context;
+        private readonly Random random;
+
+        public SeedDb(DataContext context)
+        {
+            this.context = context;
+            random = new Random();
+        }
+
+        public async Task SeedAsync()
+        {
+            await context.Database.EnsureCreatedAsync();
+
+            if (!context.Products.Any())
+            {
+                AddProduct("iPhone x");
+                AddProduct("Magic Mouse");
+                AddProduct("iwath Series 4");
+                await context.SaveChangesAsync();
+            }
+        }
+
+        private void AddProduct(string name)
+        {
+            context.Products.Add(new Product
+            {
+                Name = name,
+                Price = random.Next(1000),
+                IsAvailable = true,
+                Stock = random.Next(100)
+            });
+        }
+    }
+}
+
